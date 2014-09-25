b23f9fc86ac0677390df5c035b4e18771ec9d75a
chemacortes <chemacortes@gmail.com>
2014-09-25 02:34:59 +0200

    Añadida función de utilidad 'time' para obtener tiempos de ejecución en las pruebas de algoritmos.

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "algorithms"
 version := "1.0"
 
 scalaVersion := "2.11.2"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
--- src/main/scala/org/ch3m4/algorithms/package.scala
+++ src/main/scala/org/ch3m4/algorithms/package.scala
@@ -2,4 +2,20 @@ package org.ch3m4
 
 package object algorithms {
 
+  /*
+	 * Función de utilidad para calcular tiempos de ejecución
+	 *
+	 * Uso:
+	 *
+	 * time {
+	 * 		bloque
+	 * }
+	 */
+  def time[A](f: => A) = {
+    val s = System.nanoTime
+    val ret = f
+    println("time: " + (System.nanoTime - s) / 1e6 + "ms")
+    ret
+  } //> time: [A](f: => A)A
+
 }
