4dab78bb21f1c34a9639d10f0d0a05f20fb429e3
jmcortesa <jmcortesa@aragon.es>
2018-08-06 14:23:47 +0200

    Mejora implementación serie fibonacci

--- src/main/scala/org/ch3m4/algorithms/fibs/package.scala
+++ src/main/scala/org/ch3m4/algorithms/fibs/package.scala
@@ -8,6 +8,12 @@ package object fibs {
   /*
    * Stream de la sucesión de Fibonacci
    */
-  lazy val fibs: Stream[BigInt] = BigInt(1) #:: BigInt(1) #:: fibs.zip(fibs.tail).map(n => { n._1 + n._2 })
+  lazy val fibs: Stream[BigInt] = BigInt(1) #:: BigInt(1) #:: (fibs zip fibs.tail).map{ case (a,b) => a + b }
+
+  /*
+   * Implementación mediante scanLeft
+   */
+
+  lazy val fibsScan: Stream[BigInt] = BigInt(1) #:: fibsScan.scanLeft(BigInt(1)){case (acc, x) => acc + x}
 
 }
